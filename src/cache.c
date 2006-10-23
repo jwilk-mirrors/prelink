53d8305a5f93dac3c6b1217893e029422a2f79f4
Jakub Jelinek <jakub@redhat.com>
2006-10-23 10:27:06 +0000

    Avoid SEGFAULT when sorting cache entries (rh#197451)
    git-svn-id: svn://sourceware.org/svn/prelink/trunk@135 94c539fb-cf18-0410-b60f-edeeb537fa16

--- src/cache.c
+++ src/cache.c
@@ -336,8 +336,8 @@ deps_cmp (const void *A, const void *B)
   struct prelink_entry *a = * (struct prelink_entry **) A;
   struct prelink_entry *b = * (struct prelink_entry **) B;
 
-  if (a == NULL && b != NULL)
-    return 1;
+  if (a == NULL)
+    return (b != NULL);
   if (a != NULL && b == NULL)
     return -1;
 
