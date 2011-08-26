3e5d05e8c0148d6ca46bf12c3be3621f00d363c9
Jakub Jelinek <jakub@redhat.com>
2011-08-26 08:31:37 +0000

    * src/arch-arm.c (PL_ARCH): Change .dynamic_linker to 	/lib/ld-linux.so.3.
    git-svn-id: svn://sourceware.org/svn/prelink/trunk@195 94c539fb-cf18-0410-b60f-edeeb537fa16

--- testsuite/cxx3.sh
+++ testsuite/cxx3.sh
@@ -11,7 +11,7 @@ savelibs
 echo $PRELINK -vvvv ${PRELINK_OPTS--vm} ./cxx3 > cxx3.log
 $PRELINK -vvvv ${PRELINK_OPTS--vm} ./cxx3 >> cxx3.log 2>&1 || exit 1
 grep ^`echo $PRELINK | sed 's/ .*$/: /'` cxx3.log | grep -q -v 'C++ conflict' && exit 2
-[ $( grep ^`echo $PRELINK | sed 's/ .*$/: /'` cxx3.log | grep 'Removing C++ conflict' | wc -l ) -ge 34 ] || exit 3
+[ $( grep ^`echo $PRELINK | sed 's/ .*$/: /'` cxx3.log | grep 'Removing C++ conflict' | wc -l ) -ge 29 ] || exit 3
 LD_LIBRARY_PATH=. ./cxx3 || exit 4
 readelf -a ./cxx3 >> cxx3.log 2>&1 || exit 5
 # So that it is not prelinked again
